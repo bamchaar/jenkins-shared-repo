84824d2a959476aa9946e0f24f15bdf396874a51
DMVTCSTU <42882087+bamchaar@users.noreply.github.com>
2023-03-18 17:39:54 -0400

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,9 +2,9 @@
 
 def call(){
     echo 'Building Docker image'
-    withCredentials([usernamePassword(credentialsId: 'bamchaarJenkinsKey',passwordVariable: 'PASS' , usernameVariable: 'USER')]) {
-        sh 'docker build -t bamchaar/my-app:java-mvn-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'tcdmvkey',passwordVariable: 'PASS' , usernameVariable: 'USER')]) {
+        sh 'docker build -t btcdmv/my-app:java-mvn-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push bamchaar/my-app:java-mvn-2.0'
+        sh 'docker push tcdmv/my-app:java-mvn-2.0'
     }
 }
