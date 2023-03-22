1620cec9fefce7d71d8c2f3aa70f2f97db4520e4
DMVTCSTU <42882087+bamchaar@users.noreply.github.com>
2023-03-22 18:28:08 -0400

    Update buildImagePet.groovy

--- vars/buildImagePet.groovy
+++ vars/buildImagePet.groovy
@@ -2,11 +2,6 @@
 
 def call(){
     echo 'Building Docker image'
-    withCredentials([usernamePassword(credentialsId: 'bamchaarJenkinsKey',passwordVariable: 'PASS' , usernameVariable: 'USER')]) {
-        sh 'docker build -t bamchaar/my-app:java-mvn-2.0 .'
-    withCredentials([usernamePassword(credentialsId: 'tcdmvkey',passwordVariable: 'PASS' , usernameVariable: 'USER')]) {
-        sh 'docker build -t btcdmv/my-app:java-mvn-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
                 dir('/blob/main/docker-compose.yml') {
                     sh 'docker-compose up -d'
                 }
