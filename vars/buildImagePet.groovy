3dea5523ba3d78f12bf22647b303c85a45697e91
DMVTCSTU <42882087+bamchaar@users.noreply.github.com>
2023-03-22 18:58:59 -0400

    Update buildImagePet.groovy

--- vars/buildImagePet.groovy
+++ vars/buildImagePet.groovy
@@ -3,7 +3,6 @@
 def call(){
     echo 'Building Docker image' 
     withCredentials([usernamePassword(credentialsId: 'bamchpat',passwordVariable: 'PASS' , usernameVariable: 'USER')]) {
-        sh "echo $PASS | docker login -u $USER --password-stdin"
         git branch: 'main', url: 'https://github.com/bamchaar/petclinic-microservices-with-db.git'
         sh 'docker-compose up -d'
     }
