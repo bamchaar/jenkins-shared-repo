05034150439e625ec2caf0242dacec7997772f9e
DMVTCSTU <42882087+bamchaar@users.noreply.github.com>
2023-03-23 16:22:36 -0400

    Update buildImagePet.groovy

--- vars/buildImagePet.groovy
+++ vars/buildImagePet.groovy
@@ -5,7 +5,7 @@ def call(){
     withCredentials([usernamePassword(credentialsId: 'bamchpat',passwordVariable: 'PASS' , usernameVariable: 'USER')]) {
         git branch: 'main', url: 'https://github.com/bamchaar/petclinic-microservices-with-db.git'
         sh 'curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -'
-        sh 'sudo apt-get install -y nodejs'
+        sh 'apt-get install -y nodejs'
         sh 'npm run build'
     }
 
