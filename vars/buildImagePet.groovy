413f46eb790170bade1ce64866fffa6fee1ff83f
DMVTCSTU <42882087+bamchaar@users.noreply.github.com>
2023-03-23 16:21:32 -0400

    Update buildImagePet.groovy

--- vars/buildImagePet.groovy
+++ vars/buildImagePet.groovy
@@ -4,6 +4,8 @@ def call(){
     echo 'Building Docker image' 
     withCredentials([usernamePassword(credentialsId: 'bamchpat',passwordVariable: 'PASS' , usernameVariable: 'USER')]) {
         git branch: 'main', url: 'https://github.com/bamchaar/petclinic-microservices-with-db.git'
+        sh 'curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -'
+        sh 'sudo apt-get install -y nodejs'
         sh 'npm run build'
     }
 
